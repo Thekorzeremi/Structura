313163ad9a18d6859ddb05b500d974f0bd7c1b5e
IdrisHAMMI <hammi.idris@gmail.com>
2025-03-05 15:13:37 +0100

    updated profil api

--- backend/src/Controller/UserProfileController.php
+++ backend/src/Controller/UserProfileController.php
@@ -12,18 +12,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserProfileController extends AbstractController
 {
-
+    ###Profil & Paramètres GET API
     #[Route('/api/profile/{id}', name: 'get_profile', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
     public function getProfile(int $id, EntityManagerInterface $entityManager, Request $request): JsonResponse
     {
-        // Debug lines
         $user = $this->getUser();
         if (!$user) {
             return $this->json(['error' => 'No authenticated user found'], 401);
         }
         
-        // Original code
         $user = $entityManager->getRepository(User::class)->find($id);
         if (!$user) {
             return $this->json(['error' => 'User not found'], 404);
@@ -33,6 +31,7 @@ class UserProfileController extends AbstractController
             'id' => $user->getId(),
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName(),
+            'email' => $user->getEmail(),
             'phone' => $user->getPhone(),
             'roles' => $user->getRoles(),
             #'profile_pic' => $user->getProfilePic(),
@@ -41,7 +40,7 @@ class UserProfileController extends AbstractController
         ]);
     }
 
-
+    ###Profil & Paramètres GET API
     #[Route('/api/profile/{id}', name: 'update_profile', methods: ['PUT'])]
     #[IsGranted('ROLE_USER')]
     public function updateProfile(int $id, Request $request, EntityManagerInterface $entityManager): JsonResponse
@@ -53,11 +52,9 @@ class UserProfileController extends AbstractController
             return $this->json(['error' => 'User not found'], 404);
         }
 
-        // Only update fields that are provided
         if (isset($data['first_name'])) { $user->setFirstName($data['first_name']); }
         if (isset($data['last_name'])) { $user->setLastName($data['last_name']); }
         if (isset($data['phone'])) { $user->setPhone($data['phone']); }
-        if (isset($data['roles'])) { $user->setRoles($data['roles']); }
         #if (isset($data['profile_pic'])) { $user->setProfilePic($data['profile_pic']); }
         if (isset($data['skills'])) { $user->setSkills($data['skills']); }
         if (isset($data['job'])) { $user->setJob($data['job']); }
