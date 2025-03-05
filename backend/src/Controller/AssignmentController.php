09868f012c5a1bb9169f3a96d10f27bf142c2957
baptouu59 <baptouulereuf@gmail.com>
2025-03-05 17:27:20 +0100

    affection

--- backend/src/Controller/AssignmentController.php
+++ backend/src/Controller/AssignmentController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Affectation;
+use App\Entity\User;
 
 final class AssignmentController extends AbstractController
 {
-    #[Route('/assignment', name: 'app_assignment')]
-    public function index(): Response
+    #[Route('/api/affectations/{id}', name: 'api_affectations_by_user', methods: ['GET'])]
+    public function getAffectationsByUser(int $id, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('assignment/index.html.twig', [
-            'controller_name' => 'AssignmentController',
-        ]);
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            return $this->json(['error' => 'Utilisateur non trouvé'], 404);
+        }
+
+        $affectations = $entityManager->getRepository(Affectation::class)->findBy(['user' => $user]);
+
+        $result = array_map(fn($affectation) => [
+            'title' => $affectation->getTitle(),
+            'place' => $affectation->getPlace(),
+        ], $affectations);
+
+        return $this->json($result);
     }
 }
