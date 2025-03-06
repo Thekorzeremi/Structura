0de73f2ac735e02eb01de7c690f460afcbf6587d
Rémi KORZENIOWSKI <github.bolt438@passmail.net>
2025-03-06 05:15:42 +0100

    ci: settings init

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import Select from "../components/ui/Select";
+
 export default function Settings() {
+  const [filter, setFilter] = useState('profile');
+
+  const filterItems = [
+    { name: "Profil", value: 'profile'},
+    { name: "Paramètres", value: 'settings'}
+  ]
+  
   return (
     <div className="pl-8 pt-6">
-      <span className="text-3xl font-semibold">Paramètres</span>
+      <span className="text-3xl font-semibold">Profil & Paramètres</span>
+      <div className="mt-6 p-8 bg-white rounded-lg">
+        <Select items={filterItems} value={filter} onChange={(value) => setFilter(value)} />
+          
+      </div>
     </div>
   );
 }
